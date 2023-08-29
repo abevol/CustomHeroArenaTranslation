8c124798da35081b3a7035ddf4f4e74ae32a1219
abevol <abevol@pm.me>
2023-08-29 13:58:43 +0800

    Add project files.

---
+++ CustomHeroArenaTranslation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValveKeyValue;
+
+namespace CustomHeroArenaTranslation
+{
+    internal class Program
+    {
+        private static string ReferencesPath = @"D:\workspace\games\dota2\works\CustomHeroArenaTranslation\TranslationData\References";
+        private static string GeneratedPath = @"D:\workspace\games\dota2\works\CustomHeroArenaTranslation\TranslationData\Generated";
+
+        private class AddonLang
+        {
+            public string Language { get; set; }
+            public Dictionary<string, string> Tokens { get; set; } = new();
+        }
+
+        static void Main(string[] args)
+        {
+            var serializer = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+
+            KVObject arenaRussian = serializer.Deserialize(File.OpenRead($"{ReferencesPath}\\arena\\addon_russian.txt"), 
+                new KVSerializerOptions{ HasEscapeSequences = true }).First(x => x.Name == "Tokens");
+            KVObject chaosRussian = serializer.Deserialize(File.OpenRead($"{ReferencesPath}\\chaos\\addon_russian.txt"),
+                new KVSerializerOptions { HasEscapeSequences = true }).First(x => x.Name == "Tokens");
+            KVObject chaosSChinese = serializer.Deserialize(File.OpenRead($"{ReferencesPath}\\chaos\\addon_schinese.txt"),
+                new KVSerializerOptions { HasEscapeSequences = true }).First(x => x.Name == "Tokens");
+
+            var arenaSChinese = new AddonLang { Language = "SChinese" };
+
+            foreach (var kv in arenaRussian)
+            {
+                Console.WriteLine(kv.Name + ": " + kv.Value);
+            }
+
+            using var ws = File.OpenWrite($"{GeneratedPath}\\arena\\addon_schinese.txt");
+            serializer.Serialize(ws, arenaSChinese, "lang", new KVSerializerOptions { HasEscapeSequences = true });
+        }
+    }
+}
